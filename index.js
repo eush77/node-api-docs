92f6f09e745fccee003fef255ae457942efa9b0b
Eugene Sharygin <eush77@gmail.com>
2015-05-27 08:57:24 +0300

    Check that module is built-in

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 'use strict';
 
-var request = require('request');
+var request = require('request'),
+    builtins = require('builtins');
+
+
+var checkIsBuiltin = function (module) {
+  if (builtins.indexOf(module) < 0) {
+    throw new Error('No built-in module \'' + module + '\'');
+  }
+};
 
 
 exports.markdown = function (module) {
+  checkIsBuiltin(module);
   return request('https://raw.githubusercontent.com/nodejs/io.js/master/doc/api/' +
                  module + '.markdown');
 };
 
 
 exports.html = function (module) {
+  checkIsBuiltin(module);
   return request('https://iojs.org/api/' + module + '.html');
 };
 
 
 exports.json = function (module) {
+  checkIsBuiltin(module);
   return request('https://iojs.org/api/' + module + '.json');
 };
